0e17c29df3c9b7d2fd97b13d9a4f5f76d86204fd
annetlevitskaya <annetlevitskaya@gmail.com>
2018-11-28 00:34:44 +0300

    finish

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// function initMap() {
+//     var mapProp= {
+//         center: new google.maps.LatLng(48.104257,17.097853),
+//         zoom:18,
+//         zoomControl: false,
+//         mapTypeControl: false,
+//         scaleControl: false,
+//         streetViewControl: false,
+//         rotateControl: false,
+//         fullscreenControl: false
+//     };
+
+//     new google.maps.Map(document.getElementById("map"), mapProp);
+// }
